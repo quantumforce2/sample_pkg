58f7be28abcb4e2e483a866155422c645579f99f
quantumforce2 <mehdisarafi@gmail.com>
2019-11-23 16:46:49 -0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='mehdi_sample_project',
+    version='0.1dev',
+    packages=['mehdi_sample_pkg',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description='mehdi has a sample project'
+)
